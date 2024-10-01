43d200cdfc67bcdc1f6cb7721826e6ee660f132a
dataraj <dhadas808@yahoo.com>
2024-10-01 22:07:09 +0530

    added: express typescript project

---
+++ ticker-client/src/index.ts
@@ -0,0 +1,12 @@
+import { StockController } from './controller/stock-controller.js';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 4000;
+const stockController = new StockController();
+
+app.get('/tickers', (req, res) => stockController.fetchTickers(req, res));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
